13c3f36cb763cb4960d37efd504c558e168a7387
jmolly <roshkovskypetr@gmail.com>
2019-06-08 15:32:43 +0300

    feat: add prop types

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+//---------Cannot have state---------
+// const App = () => <h1>Hello Stateless</h1>
+
+//---------Can have state---------
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    let txt = this.props.txt;
+    return <h1>{txt}</h1>
   }
+}
+
+App.propTypes = {
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'this is the default text',
 }
 
 export default App;
